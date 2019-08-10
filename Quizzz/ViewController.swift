e3edb6e2201b352833eca96730b67209c36e55e4
Phuc Nguyen <kufuc2604@gmail.com>
2019-08-10 06:50:54 -0400

    Initial Commit

---
+++ Quizzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizzz
+//
+//  Created by Ba Phuc Nguyen on 2019-08-10.
+//  Copyright © 2019 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
